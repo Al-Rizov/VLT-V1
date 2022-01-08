60c8c5cdaa26479e527e1bc66bacbc1096fef1b7
Alexander Rizov <al.rizov@gmail.com>
2022-01-08 20:21:36 +0200

    Add Word-Cycle and Word-Randomizer functions.

--- VLT1.js
+++ VLT1.js
@@ -1,17 +1,53 @@
 let words = ['a','b','c','d','e'];
 let definitions = ['a','b','c','d','e'];
+let usedWords = '';
 let output = '';
 
 //Currently this function just cycles through each letter combination and
 //displays it in order. It should cycle through randomly.
-function wordCycler() {
+
+function wordRandomizer() {
+    
+    let usedNumbers = [];
     
     for(i=0; i<words.length; i++) {
-        output += words[i] + ' is ' + definitions[i] + '<br>';
+        
+        let newNumber = Math.floor(Math.random()*words.length);
+        if (newNumber === usedNumbers.find(element => element === newNumber)) {
+            i--
+            document.write('roll again <br>');
+        } else {
+            usedNumbers.push(newNumber);
+            document.write(newNumber+'<br>');
         }
     
+    }
 };
 
-wordCycler();
-document.write(output);
+function wordCycler() {    
+    
+    for(i=0; i<words.length; i++) {
+        document.write(`${words[i]} is ${definitions[i]} <br>`);
+        }    
+};
+
+
+//wordRandomizer();
+//wordCycler();
+
+function test() {
+    let usedNumbers = [];
+
+    for(i=0; i<words.length; i++) {
+        let newNumber = Math.floor(Math.random()*words.length);
+    if (newNumber === usedNumbers.find(element => element === newNumber)) {
+        document.write('roll again <br>');
+    } else {
+        usedNumbers.push(newNumber);
+        document.write(newNumber+'<br>');
+    }
+}
+}
+
+wordRandomizer();
 
