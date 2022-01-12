e6e238a7ce43b48dbd77b9ebe449f289d713150e
Alexander Rizov <al.rizov@gmail.com>
2022-01-12 17:13:34 +0200

    Program button to change DOM and cycle through words onscreen.

--- VLT1.js
+++ VLT1.js
@@ -5,30 +5,35 @@ let words = ['a',
 let definitions = ['a',
                 'b',
                 'c',
-                'd',];
+                'd'];
 let outputOrder = [];
 let readyList = [];
+let wordHistory = [];
 
 
 //Currently this function rolls for a number from 0 to (words.length - 1) and then stores each
 //number in an array. This array is used for setting the order in which wordCycler() shows words.
 
-function wordRandomizer() {
+function wordOrderRandomizer() {
     
-    let usedNumbers = [];
+    let numbersInList = [];
     
     for(i=0; i<words.length; i++) {
         
         let newNumber = Math.floor(Math.random()*words.length);
-        if (newNumber != usedNumbers.find(element => element === newNumber)) {
-            usedNumbers.push(newNumber);
+        if (newNumber != numbersInList.find(element => element === newNumber)) {
+            numbersInList.push(newNumber);
             
             } else {
             i--;
             }
     }
 
-    outputOrder = usedNumbers;
+    outputOrder = numbersInList;
+
+    for(i=0; i<words.length; i++) {
+        readyList.push(words[outputOrder[i]]);
+        } 
     
 };
 
@@ -37,14 +42,28 @@ function wordRandomizer() {
 
 function wordCycler() {
         
-    wordRandomizer();
-    for(i=0; i<words.length; i++) {
-        readyList += `${words[outputOrder[i]]} is ${definitions[outputOrder[i]]} <br>`;
-        } 
-    document.getElementById("paragraph").innerHTML = readyList;
-    readyList = [];
+    
+    
+
+    if(readyList.length != 0) {
+    document.getElementById("paragraph").innerHTML = readyList[0];
+    document.getElementById("button").innerHTML = "Next word.";
+    wordHistory.push(readyList[0]);
+    readyList.shift();
+    
+    } else if (wordHistory.length === words.length){
+        document.getElementById("paragraph").innerHTML = "Game over.";
+        document.getElementById("button").innerHTML = "Start again?";
+        wordHistory = [];
+        
+    } else {
+        document.getElementById("button").innerHTML = "Click on Me!";
+        document.getElementById("paragraph").innerHTML = "Paragraph.";
+        wordOrderRandomizer();
+    }
+    
 };
 
-
+wordOrderRandomizer();
 document.getElementById("button").addEventListener("click", wordCycler);
 
